e10a02641f2adf0bd0fa877db25759d9c5d1e665
Mariano D'Angelo <madang@taltech.ee>
2022-09-29 14:56:08 +0200

    REQ 2: Added greeting method overload with varargs

--- src/main/java/tdd_kata/Greeting.java
+++ src/main/java/tdd_kata/Greeting.java
@@ -7,4 +7,17 @@ public class Greeting {
         }
         return String.format("Hello, %s!", name);
     }
+
+    public String greet(String ... names) {
+        int emptynessCounter = 0;
+       for (String name : names) {
+           if (name == null || name.isEmpty() || name.isBlank()) {
+               emptynessCounter++;
+           }
+       }
+       if (emptynessCounter == names.length) {
+           return "Hello, my friends.";
+       }
+       return "";
+    }
 }
