8ac3532e4e6e761a23ae918255112ec999b6fe2f
Mariano D'Angelo <madang@taltech.ee>
2022-10-03 00:36:15 +0200

    REQ 7: Create method to check if names are split

--- src/main/java/tdd_kata/Greeting.java
+++ src/main/java/tdd_kata/Greeting.java
@@ -16,6 +16,11 @@ public class Greeting {
 
 
     public String greet(String... names) {
+       // split all the names if necessary
+        if (!areNamesSplit(names)) {
+            return "";
+        }
+
         // nameCount contains all the types of names, both shouted and unshouted, not blank, null, or "  "
         int nameCount = findDefinedNames(names).size(), unshoutedNameCount = findUnshoutedNames(names).size(),
                 shoutedNameCount = findShoutedNames(names).size(), undefinedNameCount = findUndefinedNames(names).size();
@@ -46,6 +51,15 @@ public class Greeting {
 
     // BELOW METHODS USED IN GREET METHODS
 
+
+    private static boolean areNamesSplit(String[] names) {
+        for (String name : names) {
+            if (name.contains(", ")) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     private static boolean isNameUppercase(String name) {
         if (name == null) {
