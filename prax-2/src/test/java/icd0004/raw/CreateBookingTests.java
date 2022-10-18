2c2011b4fef33f55e57bc0a237a12c33650ddad0
Mariano D'Angelo <madang@taltech.ee>
2022-10-18 21:56:24 +0300

    EX 2: Add test with post response

--- prax-2/src/test/java/icd0004/raw/CreateBookingTests.java
+++ prax-2/src/test/java/icd0004/raw/CreateBookingTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 public class CreateBookingTests {
     public static final String API_URL = "https://restful-booker.herokuapp.com/booking/";
@@ -34,5 +33,30 @@ public class CreateBookingTests {
                 .statusCode(200)
                 .body("bookingid", notNullValue());
 
+    }
+
+    @Test
+    public void postBookingResponseShouldContainBooking() {
+        String payload = "{\n" +
+                "    \"firstname\": \"Al Faysal Bin\",\n" +
+                "    \"lastname\": \"Asad\",\n" +
+                "    \"totalprice\": 111,\n" +
+                "    \"depositpaid\": true,\n" +
+                "    \"bookingdates\": {\n" +
+                "    \"checkin\": \"2022-10-15\",\n" +
+                "    \"checkout\": \"2022-10-18\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\": \"Breakfast\"\n" +
+                "}";
+
+        given()
+                .accept(JSON.toString())
+                .contentType(JSON.toString())
+                .body(payload)
+                .when()
+                .post(API_URL)
+                .then()
+                .body("booking.firstname", equalTo("Al Faysal Bin"))
+                .body("booking.lastname", equalTo("Asad"));
     }
 }
