93d9fba2460c093c78ded9e7b30af855aced1846
Mariano D'Angelo <madang@taltech.ee>
2022-10-18 21:20:30 +0300

    EX 2: Add first test

--- prax-2/src/test/java/icd0004/raw/SmokeTests.java
+++ prax-2/src/test/java/icd0004/raw/SmokeTests.java
@@ -1,5 +1,18 @@
 package icd0004.raw;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+
 public class SmokeTests {
 
+    public static final String API_URL = "https://restful-booker.herokuapp.com/booking";
+
+    @Test
+    public void getAllBookingsShouldReturn200() {
+        when()
+                .get(API_URL)
+                .then()
+                .statusCode(200);
+    }
 }
