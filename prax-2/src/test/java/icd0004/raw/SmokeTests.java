d827da73e6b9418f95e7e1f431b24a716dd588fb
Mariano D'Angelo <madang@taltech.ee>
2022-10-18 21:24:29 +0300

    EX 2: Add second test

--- prax-2/src/test/java/icd0004/raw/SmokeTests.java
+++ prax-2/src/test/java/icd0004/raw/SmokeTests.java
@@ -2,7 +2,9 @@ package icd0004.raw;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static io.restassured.http.ContentType.JSON;
 
 public class SmokeTests {
 
@@ -12,6 +14,16 @@ public class SmokeTests {
     public void getAllBookingsShouldReturn200() {
         when()
                 .get(API_URL)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void getBookingIdShouldReturn200() {
+        given()
+                .contentType(JSON.toString())
+                .when()
+                .get(API_URL + "/121")
                 .then()
                 .statusCode(200);
     }
