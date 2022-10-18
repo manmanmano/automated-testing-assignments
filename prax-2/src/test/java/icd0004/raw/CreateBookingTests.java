64ff55b8b58cdb156f19836700cf41a1446fdd19
Mariano D'Angelo <madang@taltech.ee>
2022-10-18 21:48:51 +0300

    EX 2: Add test with post response

--- prax-2/src/test/java/icd0004/raw/CreateBookingTests.java
+++ prax-2/src/test/java/icd0004/raw/CreateBookingTests.java
@@ -1,5 +1,38 @@
 package icd0004.raw;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
 public class CreateBookingTests {
     public static final String API_URL = "https://restful-booker.herokuapp.com/booking/";
+
+    @Test
+    public void postBookingResponseShouldContainBookingId() {
+        String payload = "{\n" +
+                "    \"firstname\": \"Al Faysal Bin\",\n" +
+                "    \"lastname\": \"Asad\",\n" +
+                "    \"totalprice\": 111,\n" +
+                "    \"depositpaid\": true,\n" +
+                "    \"bookingdates\": {\n" +
+                "    \"checkin\": \"2022-10-15\",\n" +
+                "    \"checkout\": \"2022-10-18\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\": \"Breakfast\"\n" +
+                "}";
+
+        given()
+                .contentType(JSON.toString())
+                .accept(JSON.toString())
+                .body(payload)
+                .when()
+                .post(API_URL)
+                .then()
+                .statusCode(200)
+                .body("bookingid", notNullValue());
+
+    }
 }
