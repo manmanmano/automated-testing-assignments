091f0032d50d35c35d80de04e7f1657acd1d8e33
Mariano D'Angelo <madang@taltech.ee>
2022-10-18 19:14:42 +0300

    git init

---
+++ prax-2/src/main/java/ee/icd0004/madang/App.java
@@ -0,0 +1,13 @@
+package ee.icd0004.madang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
