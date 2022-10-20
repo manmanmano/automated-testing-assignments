743f26e953b4849871eabcee2159c1a594c1d494
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 23:40:37 +0300

    EX 3: Implement postCredentials, tests passing

--- prax-2/src/main/java/icd0004/framework/AuthenticationApi.java
+++ prax-2/src/main/java/icd0004/framework/AuthenticationApi.java
@@ -1,12 +1,24 @@
 package icd0004.framework;
 
+import icd0004.framework.request.Authentication;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 
 public class AuthenticationApi {
     private static final String BASE_URL = "https://restful-booker.herokuapp.com";
     private static final String AUTH_API = BASE_URL + "/auth";
 
-    public static Response postCredentials(Authentication payload) {
-
+    public static Response postCredentials(Authentication payload, String username, String password) {
+        return given()
+                .auth()
+                .preemptive()
+                .basic(username, password)
+                .body(payload)
+                .contentType(ContentType.JSON)
+                .when()
+                .post(AUTH_API);
     }
 }
