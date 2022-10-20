1df39b562b23d4ac1f144ea93b6a6d65d9b8e6d7
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 23:58:57 +0300

    EX 3: Improve test

--- prax-2/src/test/java/icd0004/framework/CreateAuthenticationTests.java
+++ prax-2/src/test/java/icd0004/framework/CreateAuthenticationTests.java
@@ -1,12 +1,22 @@
 package icd0004.framework;
 
 import icd0004.framework.request.Authentication;
+import icd0004.framework.response.AuthenticationResponse;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CreateAuthenticationTests {
 
     @Test
     public void postAuthenticationWithIncorrectCredentialsReturnBadCredentials() {
         Authentication credentials = Authentication.getCredentials();
+
+        AuthenticationResponse authenticationResponse = AuthenticationApi
+                .postCredentials(credentials)
+                .as(AuthenticationResponse.class);
+
+        assertThat(authenticationResponse.getAuth())
+                .as("Bad credentials");
     }
 }
