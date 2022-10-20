d6357a1273151745f864ba9fe98172f7095fe15d
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 18:00:29 +0300

    EX 3: Add new packages to main request and restore. Create and initiate Booking.java

---
+++ prax-2/src/main/java/icd0004/framework/request/Booking.java
@@ -0,0 +1,11 @@
+package icd0004.framework.request;
+
+public class Booking {
+    private String firstname;
+    private String lastname;
+    private int totalprice;
+    private boolean depositpaid;
+    private String additionalneeds;
+    private BookingDates bookingdates;
+
+}
