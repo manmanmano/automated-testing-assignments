e5301c4c782686a40ada7a02ff80c7443dfcb601
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 23:29:16 +0300

    EX 3: Add implementation

--- prax-2/src/main/java/icd0004/framework/request/Authentication.java
+++ prax-2/src/main/java/icd0004/framework/request/Authentication.java
@@ -1,6 +1,27 @@
 package icd0004.framework.request;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonAutoDetect
+@Builder
 public class Authentication {
     private String username;
     private String password;
+
+    public static Authentication getCredentials() {
+        Faker faker = new Faker();
+
+        return Authentication.builder()
+                .username(faker.name().username())
+                .password(faker.rickAndMorty().character())
+                .build();
+    }
 }
