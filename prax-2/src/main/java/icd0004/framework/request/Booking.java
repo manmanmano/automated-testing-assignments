8f9a325d41a2f2b4064e64e931afd780a9a6050f
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 18:08:33 +0300

    EX 3: create getFullPayload method in Booking.java

--- prax-2/src/main/java/icd0004/framework/request/Booking.java
+++ prax-2/src/main/java/icd0004/framework/request/Booking.java
@@ -1,14 +1,19 @@
 package icd0004.framework.request;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.github.javafaker.Faker;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import static java.util.concurrent.TimeUnit.DAYS;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonAutoDetect
+@Builder
 public class Booking {
     private String firstname;
     private String lastname;
@@ -17,4 +22,19 @@ public class Booking {
     private String additionalneeds;
     private BookingDates bookingdates;
 
+    public static Booking getFullPayload() {
+        Faker faker = new Faker();
+        BookingDates bookingDates = new BookingDates();
+        bookingDates.setCheckin(faker.date().future(1, DAYS));
+        bookingDates.setCheckout(faker.date().future(3, DAYS));
+
+        return Booking.builder()
+                .firstname(faker.name().firstName())
+                .lastname(faker.name().lastName())
+                .totalprice(faker.number().randomDigit())
+                .depositpaid(true)
+                .bookingdates(bookingDates)
+                .additionalneeds(faker.howIMetYourMother().catchPhrase())
+                .build();
+    }
 }
