fbd1c9f91bd79ed6535513f147e67362fc352200
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 17:45:07 +0300

    Fix scope for some dependencies

--- prax-2/src/main/java/icd0004/framework/BookingApi.java
+++ prax-2/src/main/java/icd0004/framework/BookingApi.java
@@ -1,4 +1,11 @@
 package icd0004.framework;
 
+
 public class BookingApi {
+    private static final String BASE_URL = "https://restful-booker.herokuapp.com";
+    private static final String BOOKING_API = BASE_URL + "/booking/";
+
+    public static Response getBookings() {
+        return given().get(BOOKING_API);
+    }
 }
