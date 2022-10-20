33a8b28560eec28d3325e104f302f6d2fa6cb150
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 23:23:07 +0300

    EX 3: Start implementation

--- prax-2/src/main/java/icd0004/framework/AuthenticationApi.java
+++ prax-2/src/main/java/icd0004/framework/AuthenticationApi.java
@@ -1,5 +1,12 @@
 package icd0004.framework;
 
-public class AuthenticationApi {
+import io.restassured.response.Response;
 
+public class AuthenticationApi {
+    private static final String BASE_URL = "https://restful-booker.herokuapp.com";
+    private static final String AUTH_API = BASE_URL + "/auth";
+
+    public static Response postCredentials(Authentication payload) {
+
+    }
 }
