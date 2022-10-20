70bfb16bf6ec2738601fa5810051e705187595b1
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 17:30:44 +0300

    EX 3: Add faulty smoke test

--- prax-2/src/test/java/icd0004/framework/SmokeTests.java
+++ prax-2/src/test/java/icd0004/framework/SmokeTests.java
@@ -1,5 +1,23 @@
 package icd0004.framework;
 
+import org.junit.jupiter.api.Test;
+
 public class SmokeTests {
 
+    @Test
+    public void getAllBookingsShouldReturnHttp200() {
+        BookingApi.getBookings().then().statusCode(200);
+    }
+
+    @Test
+    public void getBookingIdShouldReturnHttp200() {
+        BookingApi.getBookingById(121).then().statusCode(200);
+    }
+
+    @Test
+    public void postBookingShouldReturnHttp200() {
+        Booking bookingPayload = Booking.getFullPayload();
+
+        BookingResponse bookingResponse = BookingApi.postBooking(bookingPayload).then().statusCode(200);
+    }
 }
