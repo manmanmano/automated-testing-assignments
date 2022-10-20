eb9fcaac978d699468320ef67ba4458b6dc7b1d6
Mariano D'Angelo <madang@taltech.ee>
2022-10-20 18:49:59 +0300

    EX 3: Create method buildWithResource

--- prax-2/src/main/java/icd0004/framework/request/Booking.java
+++ prax-2/src/main/java/icd0004/framework/request/Booking.java
@@ -1,11 +1,15 @@
 package icd0004.framework.request;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import static java.util.concurrent.TimeUnit.DAYS;
 
@@ -36,5 +40,12 @@ public class Booking {
                 .bookingdates(bookingDates)
                 .additionalneeds(faker.howIMetYourMother().catchPhrase())
                 .build();
+    }
+
+    public static Booking buildWithResource(String filePath) throws IOException {
+        ClassLoader classLoader = Booking.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(filePath);
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(inputStream, Booking.class);
     }
 }
