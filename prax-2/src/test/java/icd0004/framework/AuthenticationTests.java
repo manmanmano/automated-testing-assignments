4a8f04ba70697505e03627b870c30ecc871b43a6
Mariano D'Angelo <madang@taltech.ee>
2022-10-24 01:25:44 +0300

    Rename file

--- prax-2/src/test/java/icd0004/framework/AuthenticationTests.java
+++ prax-2/src/test/java/icd0004/framework/AuthenticationTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class CreateAuthenticationTests {
+public class AuthenticationTests {
 
     @Test
     public void postAuthenticationWithIncorrectCredentialsShouldReturnBadCredentials() {
