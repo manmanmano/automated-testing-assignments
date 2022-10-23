369db12181c788255ab618e30911992dd03e9488
Mariano D'Angelo <madang@taltech.ee>
2022-10-24 00:31:50 +0300

    EX 3: Remove unecessary overriding funtion

--- prax-2/src/main/java/icd0004/framework/AuthenticationApi.java
+++ prax-2/src/main/java/icd0004/framework/AuthenticationApi.java
@@ -11,17 +11,6 @@ public class AuthenticationApi {
     private static final String BASE_URL = "https://restful-booker.herokuapp.com";
     private static final String AUTH_API = BASE_URL + "/auth";
 
-    public static Response postCredentials(Authentication payload, String username, String password) {
-        return given()
-                .auth()
-                .preemptive()
-                .basic(username, password)
-                .body(payload)
-                .contentType(ContentType.JSON)
-                .when()
-                .post(AUTH_API);
-    }
-
     public static Response postCredentials(Authentication payload) {
         return given()
                 .auth()
