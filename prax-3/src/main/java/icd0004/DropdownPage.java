eff0a2626ecafe39f3e03ab903bdc222f59380cf
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:33:52 +0200

    Selenide with pom: Add implementation

--- prax-3/src/main/java/icd0004/DropdownPage.java
+++ prax-3/src/main/java/icd0004/DropdownPage.java
@@ -1,4 +1,21 @@
 package icd0004;
 
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class DropdownPage {
+
+    private final By dropdownPageLink = By.linkText("Dropdown");
+    private final By dropdownField = By.id("dropdown");
+
+    public void goTo() {
+        open("/");
+        $(dropdownPageLink).click();
+    }
+
+    public String getPageTitleText() {
+        return $("h3").getText();
+    }
 }
