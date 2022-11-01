4c13665eafb2614f3604e9cfdb81c717fa63aee7
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:14:45 +0200

    Selenide with pom: Create login method

--- prax-3/src/main/java/icd0004/FormAuthenticationPage.java
+++ prax-3/src/main/java/icd0004/FormAuthenticationPage.java
@@ -8,6 +8,9 @@ import static com.codeborne.selenide.Selenide.open;
 public class FormAuthenticationPage {
 
     private final By dropdownPageLink = By.linkText("Form Authentication");
+    private final By usernameField = By.id("username");
+    private final By passwordField = By.id("password");
+    private final By submitButton = By.xpath("//button");
 
     public void goTo() {
         open("/");
@@ -16,5 +19,11 @@ public class FormAuthenticationPage {
 
     public String getPageTitleText() {
         return $("h2").getText();
+    }
+
+    public void loginWithCredentials(String username, String password) {
+        $(usernameField).setValue(username);
+        $(passwordField).setValue(password);
+        $(submitButton).click();
     }
 }
