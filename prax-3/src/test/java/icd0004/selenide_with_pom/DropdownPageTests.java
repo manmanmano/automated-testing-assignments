2b5b4d846901292be6eadad1915e9c1237777d15
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:31:04 +0200

    Selenide with pom: Add faulty navigation and test

--- prax-3/src/test/java/icd0004/selenide_with_pom/DropdownPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_with_pom/DropdownPageTests.java
@@ -1,4 +1,23 @@
 package icd0004.selenide_with_pom;
 
-public class DropdownPageTests {
+import icd0004.DropdownPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DropdownPageTests extends BaseTest {
+
+    private static DropdownPage dropdownPage;
+
+    @BeforeEach
+    public void goToDropDownPage() {
+        dropdownPage = new DropdownPage();
+        dropdownPage.goTo();
+    }
+
+    @Test
+    public void canOpenDropdownPage() {
+        assertThat(dropdownPage.getPageTitleText()).isEqualTo("Dropdown List");
+    }
 }
