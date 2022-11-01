198d5a89719bfb9f0ffc5d3320a4d7bc532e11ab
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 18:42:31 +0200

    Selenide with pom: Add final strings in implementation

--- prax-3/src/main/java/icd0004/DynamicLoadingPage.java
+++ prax-3/src/main/java/icd0004/DynamicLoadingPage.java
@@ -8,6 +8,11 @@ import static com.codeborne.selenide.Selenide.open;
 public class DynamicLoadingPage {
 
     private final By dynamicLoadingPageLink = By.linkText("Dynamic Loading");
+    private final By exampleOneLink = By.linkText("Example 1: Element on page that is hidden");
+    private final By exampleTwoLink = By.linkText("Example 2: Element rendered after the fact");
+    private final By startButton = By.xpath("//button[@type='submit']");
+    private final By greetingField = By.tagName("h4");
+
     public void goTo() {
         open("/");
         $(dynamicLoadingPageLink).click();
