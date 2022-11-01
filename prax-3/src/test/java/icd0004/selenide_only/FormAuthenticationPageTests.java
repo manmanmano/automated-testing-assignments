8576fb17f18bacc82d0f4acad28a22528d7dd187
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 17:23:07 +0200

    Selenide only: Add 'default' test

--- prax-3/src/test/java/icd0004/selenide_only/FormAuthenticationPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_only/FormAuthenticationPageTests.java
@@ -1,8 +1,10 @@
 package icd0004.selenide_only;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,5 +13,10 @@ public class FormAuthenticationPageTests extends BaseTest {
     public void navigateToPage() {
         open("/");
         $(By.linkText("Form Authentication")).click();
+    }
+
+    @Test
+    public void canOpenFormAuthenticationPage() {
+        $("h2").shouldHave(text("Login Page"));
     }
 }
