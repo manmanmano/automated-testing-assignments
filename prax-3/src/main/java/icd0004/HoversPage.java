39453609b10e36a043deee5b5b1608171eb8576e
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:52:40 +0200

    Selenide with pom: Add implementation

--- prax-3/src/main/java/icd0004/HoversPage.java
+++ prax-3/src/main/java/icd0004/HoversPage.java
@@ -1,4 +1,20 @@
 package icd0004;
 
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class HoversPage {
+
+   private final By hoversPageLink = By.linkText("Hovers");
+
+    public void goTo() {
+        open("/");
+        $(hoversPageLink).click();
+    }
+
+    public String getPageTitleText() {
+        return $("h3").getText();
+    }
 }
