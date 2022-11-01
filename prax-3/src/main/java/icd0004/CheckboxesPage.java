6ca4787bb66c7769400e959131bd771d4c5683bf
Mariano D'Angelo <madang@taltech.ee>
2022-11-02 00:45:27 +0200

    Selenide with pom: Add implementation

--- prax-3/src/main/java/icd0004/CheckboxesPage.java
+++ prax-3/src/main/java/icd0004/CheckboxesPage.java
@@ -1,4 +1,20 @@
 package icd0004;
 
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class CheckboxesPage {
+
+   private final By checkboxesPageLink = By.linkText("Checkboxes");
+
+    public void goTo() {
+        open("/");
+        $(checkboxesPageLink).click();
+    }
+
+    public String getPageTitleText() {
+        return $("h3").getText();
+    }
 }
