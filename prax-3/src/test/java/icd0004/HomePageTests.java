9155e2e1cd72e69f388ae1d146455f700b0e2ab6
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 14:24:02 +0200

    Selenide only: Add first test

---
+++ prax-3/src/test/java/icd0004/HomePageTests.java
@@ -0,0 +1,17 @@
+package icd0004;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class HomePageTests extends BaseTest {
+
+    @Test
+    public void canOpenHomePage() {
+        open("/");
+        $("h1").shouldHave(text("Welcome to the-internet"));
+    }
+
+}
