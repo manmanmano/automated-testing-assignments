7c62639087f8548fbd3c7715cfaac8823fbff6b7
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 18:36:01 +0200

    Selenide with pom: Add class extension

--- prax-3/src/test/java/icd0004/selenide_with_pom/DynamicLoadingPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_with_pom/DynamicLoadingPageTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DynamicLoadingPageTests {
+public class DynamicLoadingPageTests extends BaseTest {
 
     private static DynamicLoadingPage dynamicLoadingPage;
 
