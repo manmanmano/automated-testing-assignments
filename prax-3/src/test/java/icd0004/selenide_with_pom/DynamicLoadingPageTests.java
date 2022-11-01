91bd35c43646ec74ec0f5ade7a410334ce22bac4
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 18:18:18 +0200

    Selenide with pom: Add failing navigation

--- prax-3/src/test/java/icd0004/selenide_with_pom/DynamicLoadingPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_with_pom/DynamicLoadingPageTests.java
@@ -1,4 +1,15 @@
 package icd0004.selenide_with_pom;
 
+import icd0004.DynamicLoadingPage;
+import org.junit.jupiter.api.BeforeEach;
+
 public class DynamicLoadingPageTests {
+
+    private static DynamicLoadingPage dynamicLoadingPage;
+
+    @BeforeEach
+    public void goToDynamicLoadingPage() {
+        dynamicLoadingPage = new DynamicLoadingPage();
+        dynamicLoadingPage.goTo();
+    }
 }
