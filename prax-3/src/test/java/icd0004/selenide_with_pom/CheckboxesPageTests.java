b8ba241481d33ef16f0694774ca6d1bcfd9cd534
Mariano D'Angelo <madang@taltech.ee>
2022-11-02 01:57:07 +0200

    Selenide with pom: Edit test

--- prax-3/src/test/java/icd0004/selenide_with_pom/CheckboxesPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_with_pom/CheckboxesPageTests.java
@@ -23,7 +23,7 @@ public class CheckboxesPageTests extends BaseTest {
 
     @Test
     public void shouldSelectAllCheckboxes() {
-//        checkboxesPage.selectAllCheckboxes();
+        checkboxesPage.selectAllCheckboxes();
 
         assertThat(checkboxesPage.countCheckedCheckboxes()).isEqualTo(2);
     }
