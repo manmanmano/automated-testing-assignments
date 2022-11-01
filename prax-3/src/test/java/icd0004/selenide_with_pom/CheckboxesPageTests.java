0911d7a9bb0f8d84a8458cd8f4e7e46de4fcd88b
Mariano D'Angelo <madang@taltech.ee>
2022-11-02 00:41:53 +0200

    Selenide with pom: Add faulty navigation and test

--- prax-3/src/test/java/icd0004/selenide_with_pom/CheckboxesPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_with_pom/CheckboxesPageTests.java
@@ -1,5 +1,23 @@
 package icd0004.selenide_with_pom;
 
+import icd0004.CheckboxesPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CheckboxesPageTests extends BaseTest {
 
+    private static CheckboxesPage checkboxesPage;
+
+    @BeforeEach
+    public void goToCheckoxesPage() {
+        checkboxesPage = new CheckboxesPage();
+        checkboxesPage.goTo();
+    }
+
+    @Test
+    public void canOpenCheckboxesPage() {
+        assertThat(checkboxesPage.getPageTitleText()).isEqualTo("Checkboxes");
+    }
 }
