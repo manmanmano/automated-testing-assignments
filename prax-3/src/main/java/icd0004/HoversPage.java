85deca5c1762d27d2aabc0a1e02cc93741095bda
Mariano D'Angelo <madang@taltech.ee>
2022-11-02 00:13:37 +0200

    Selenide with pom: Add implementation

--- prax-3/src/main/java/icd0004/HoversPage.java
+++ prax-3/src/main/java/icd0004/HoversPage.java
@@ -2,12 +2,14 @@ package icd0004;
 
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 
 public class HoversPage {
 
    private final By hoversPageLink = By.linkText("Hovers");
+   private final By hoversUsersList = By.cssSelector(".figure");
+   private final By hoversSpecificUserName = By.cssSelector(".figcaption > h5");
 
     public void goTo() {
         open("/");
@@ -16,5 +18,10 @@ public class HoversPage {
 
     public String getPageTitleText() {
         return $("h3").getText();
+    }
+
+    public String getUserNameOnHover(int userIndex) {
+        return $$(hoversUsersList).get(userIndex).hover()
+                .$(hoversSpecificUserName).shouldBe(visible).getText();
     }
 }
