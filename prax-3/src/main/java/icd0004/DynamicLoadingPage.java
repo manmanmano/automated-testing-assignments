0985f873184a76b6297cf65146d6514779c79298
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 18:36:25 +0200

    Selenide with pom: Add implementation

--- prax-3/src/main/java/icd0004/DynamicLoadingPage.java
+++ prax-3/src/main/java/icd0004/DynamicLoadingPage.java
@@ -1,4 +1,19 @@
 package icd0004;
 
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class DynamicLoadingPage {
+
+    private final By dynamicLoadingPageLink = By.linkText("Dynamic Loading");
+    public void goTo() {
+        open("/");
+        $(dynamicLoadingPageLink).click();
+    }
+
+    public String getPageTitleText() {
+        return $("h3").getText();
+    }
 }
