44f0fd1f049debbadc5281046ee2c54703028a35
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:05:37 +0200

    Selenide with pom: Add failing navigation and first faulty test

--- prax-3/src/test/java/icd0004/selenide_with_pom/FormAuthenticationPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_with_pom/FormAuthenticationPageTests.java
@@ -1,4 +1,21 @@
 package icd0004.selenide_with_pom;
 
-public class FormAuthenticationPageTests {
+import icd0004.FormAuthenticationPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FormAuthenticationPageTests extends BaseTest {
+
+    private static FormAuthenticationPage formAuthenticationPage;
+
+    @BeforeEach
+    public void goToFormAuthenticationPage() {
+        formAuthenticationPage = new FormAuthenticationPage();
+        formAuthenticationPage.goTo();
+    }
+
+    @Test
+    public void canOpenFormAuthenticationPage() {
+        assertThat(formAuthenticationPage.getPageTitleText()).isEqualTo("Login Page");
+    }
 }
