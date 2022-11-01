d307358ccd48638aec720881c787fa889ef95383
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:43:07 +0200

    Selenide with pom: Add tests files

---
+++ prax-3/src/test/java/icd0004/selenide_with_pom/CheckboxesPageTest.java
@@ -0,0 +1,5 @@
+package icd0004.selenide_with_pom;
+
+public class CheckboxesPageTest extends BaseTest {
+
+}
---
+++ prax-3/src/test/java/icd0004/selenide_with_pom/HoversPageTests.java
@@ -0,0 +1,5 @@
+package icd0004.selenide_with_pom;
+
+public class HoversPageTests extends BaseTest {
+
+}
