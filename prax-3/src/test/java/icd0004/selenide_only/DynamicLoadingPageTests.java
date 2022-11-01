24302d904d0497dd172f9dd1ab1481dd2bae6fe4
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 17:51:51 +0200

    Selenide only: Create DynamicLoadingPageTests. Add navigation

--- prax-3/src/test/java/icd0004/selenide_only/DynamicLoadingPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_only/DynamicLoadingPageTests.java
@@ -1,2 +1,17 @@
-package icd0004.selenide_only;public class DynamicLoadingPageTests {
+package icd0004.selenide_only;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DynamicLoadingPageTests extends BaseTest {
+
+    @BeforeEach
+    public void navigateToPage() {
+        open("/");
+        $(By.linkText("Dynamic Loading")).click();
+    }
+
 }
