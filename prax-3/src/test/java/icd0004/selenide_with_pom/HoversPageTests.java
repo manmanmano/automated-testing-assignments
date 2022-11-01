ff7d81c505563a6a1067de23642fe021fc37ecd7
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:49:13 +0200

    Selenide with pom: Add faulty navigation and test

--- prax-3/src/test/java/icd0004/selenide_with_pom/HoversPageTests.java
+++ prax-3/src/test/java/icd0004/selenide_with_pom/HoversPageTests.java
@@ -1,5 +1,23 @@
 package icd0004.selenide_with_pom;
 
+import icd0004.HoversPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HoversPageTests extends BaseTest {
 
+    private static HoversPage hoversPage;
+
+    @BeforeEach
+    public void goToHoversPage() {
+        hoversPage = new HoversPage();
+        hoversPage.goTo();
+    }
+
+    @Test
+    public void canOpenHoversPage() {
+        assertThat(hoversPage.getPageTitleText()).isEqualTo("Hovers");
+    }
 }
