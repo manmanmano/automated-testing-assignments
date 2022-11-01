d1790b42e098108d1568d08039377a1fa65ae810
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 18:49:45 +0200

    Selenide with pom: Edit final greetingField

--- prax-3/src/main/java/icd0004/DynamicLoadingPage.java
+++ prax-3/src/main/java/icd0004/DynamicLoadingPage.java
@@ -14,7 +14,7 @@ public class DynamicLoadingPage {
     private final By exampleOneLink = By.linkText("Example 1: Element on page that is hidden");
     private final By exampleTwoLink = By.linkText("Example 2: Element rendered after the fact");
     private final By startButton = By.xpath("//button[@type='submit']");
-    private final By greetingField = By.tagName("h4");
+    private final By greetingField = By.cssSelector("#finish > h4");
 
     public void goTo() {
         open("/");
