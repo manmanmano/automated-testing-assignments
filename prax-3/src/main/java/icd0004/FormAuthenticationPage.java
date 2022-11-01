ecec3fdd42ad1b3efff2d49e77b1e045cdbe33fd
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:07:32 +0200

    Selenide with pom: Fix navigation

--- prax-3/src/main/java/icd0004/FormAuthenticationPage.java
+++ prax-3/src/main/java/icd0004/FormAuthenticationPage.java
@@ -1,4 +1,16 @@
 package icd0004;
 
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class FormAuthenticationPage {
+
+    private final By dropdownPageLink = By.linkText("Form Authentication");
+
+    public void goTo() {
+        open("/");
+        $(dropdownPageLink).click();
+    }
 }
