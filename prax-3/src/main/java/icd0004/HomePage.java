b4aaa48bff70af601b617b9b896495878b58b602
Mariano D'Angelo <madang@taltech.ee>
2022-11-01 19:26:56 +0200

    Selenide with pom: Add implementation

--- prax-3/src/main/java/icd0004/HomePage.java
+++ prax-3/src/main/java/icd0004/HomePage.java
@@ -1,4 +1,14 @@
 package icd0004;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class HomePage {
+    public void goTo() {
+        open("/");
+    }
+
+    public String getPageTitleText() {
+        return $("h1").getText();
+    }
 }
